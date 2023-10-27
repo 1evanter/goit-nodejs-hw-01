24c58e6b84ab46748f0c55db5ed691dfe88af298
1evanter <rita.oleksenkoo@gmail.com>
2023-10-27 14:02:59 +0300

    Create contacts.js

---
+++ contacts.js
@@ -0,0 +1,41 @@
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contactById = contacts.find((item) => item.id === contactId);
+
+  return contactById || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [removedContact] = contacts.slice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return removedContact;
+};
+
+const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
